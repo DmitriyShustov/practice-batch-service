ef8fe36ee5519a7f5e7a13d0902d6a6f342b4215
shustov-dmitry <shustov.dm.s@gmail.com>
2025-06-24 11:58:38 +0300

    fix: add ComponentScan in app

--- src/main/java/ru/axiomatika/batch_request/BatchServiceApplication.java
+++ src/main/java/ru/axiomatika/batch_request/BatchServiceApplication.java
@@ -2,8 +2,10 @@ package ru.axiomatika.batch_request;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"ru.axiomatika.batch_service.web.mappers", "ru.axiomatika.batch_service"})
 public class BatchServiceApplication {
 
 	public static void main(String[] args) {
