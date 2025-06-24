a60f49a24e86a7173154021ac20bf6543b091036
shustov-dmitry <shustov.dm.s@gmail.com>
2025-06-24 09:32:51 +0300

    init commit

---
+++ src/test/java/ru/axiomatika/batch_request/BatchRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.axiomatika.batch_request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
