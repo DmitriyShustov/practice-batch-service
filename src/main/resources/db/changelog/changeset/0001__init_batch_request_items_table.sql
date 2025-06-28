a0b3452c280235f70c3610e39895ac627f1449ec
shustov-dmitry <shustov.dm.s@gmail.com>
2025-06-28 17:19:06 +0300

    feat: add second table

---
+++ src/main/resources/db/changelog/changeset/0001__init_batch_request_items_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS batch_request_items (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    xml_content TEXT,
+    status SMALLINT NOT NULL,
+    batch_request_id INT NOT NULL,
+
+    CONSTRAINT fk_batch_request_items_batch_requests
+    FOREIGN KEY (batch_request_id)
+    REFERENCES batch_requests(id)
+    ON DELETE CASCADE
+);
