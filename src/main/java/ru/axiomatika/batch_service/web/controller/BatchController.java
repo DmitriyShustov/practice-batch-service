cc3d7644c29218a4b611d03f614731aa4d4492a4
shustov-dmitry <shustov.dm.s@gmail.com>
2025-06-28 13:41:43 +0300

    feat: call service in controller

--- src/main/java/ru/axiomatika/batch_service/web/controller/BatchController.java
+++ src/main/java/ru/axiomatika/batch_service/web/controller/BatchController.java
@@ -5,16 +5,22 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import ru.axiomatika.batch_service.core.service.BatchService;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class BatchController {
 
+    private final BatchService batchService;
+
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadZipStream(
             @RequestParam("file") MultipartFile file) throws Exception {
 
+        batchService.processArchive(file);
+
         return null;
     }
+
 }
