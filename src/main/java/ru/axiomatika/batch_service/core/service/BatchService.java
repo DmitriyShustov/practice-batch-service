d44666dfb82dc83a9e5ff6a9a9b512e774ee95b9
shustov-dmitry <shustov.dm.s@gmail.com>
2025-06-28 12:17:42 +0300

    feat: add empty service

---
+++ src/main/java/ru/axiomatika/batch_service/core/service/BatchService.java
@@ -0,0 +1,18 @@
+package ru.axiomatika.batch_service.core.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.axiomatika.batch_service.core.validator.BatchValidator;
+
+@Service
+@RequiredArgsConstructor
+public class BatchService {
+
+    private final BatchValidator batchValidator;
+
+    public void processArchive(MultipartFile file) {
+
+    }
+
+}
