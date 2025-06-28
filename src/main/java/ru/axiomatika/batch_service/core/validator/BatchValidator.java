a751462e31d6d58736e83fcd0453518dc5017cbe
shustov-dmitry <shustov.dm.s@gmail.com>
2025-06-28 13:23:50 +0300

    feat: add validation for empty archive

--- src/main/java/ru/axiomatika/batch_service/core/validator/BatchValidator.java
+++ src/main/java/ru/axiomatika/batch_service/core/validator/BatchValidator.java
@@ -2,8 +2,10 @@ package ru.axiomatika.batch_service.core.validator;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import ru.axiomatika.batch_service.core.exception.BaseExceptionCode;
 import ru.axiomatika.batch_service.core.exception.ValidationException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -12,8 +14,19 @@ public class BatchValidator {
     private List<ValidationException> errors;
 
     public List<ValidationException> validateArchive(MultipartFile file) {
+        this.errors = new ArrayList<>();
+
+        validateNotEmptyArchive(file);
 
         return this.errors;
     }
 
+    private void validateNotEmptyArchive(MultipartFile file) {
+        if (file.isEmpty()) {
+            this.errors.add(new ValidationException(
+                    BaseExceptionCode.INVALID_ARCHIVE_EMPTY,
+                    "Archive file is empty"
+            ));
+        }
+    }
 }
