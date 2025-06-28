c46e0183d740f70b4cd573c42db9ca2dc8a6b81d
shustov-dmitry <shustov.dm.s@gmail.com>
2025-06-28 13:16:16 +0300

    feat: handle group exception

--- src/main/java/ru/axiomatika/batch_service/web/controller/GlobalExceptionHandler.java
+++ src/main/java/ru/axiomatika/batch_service/web/controller/GlobalExceptionHandler.java
@@ -6,7 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.axiomatika.batch_service.core.exception.BaseException;
+import ru.axiomatika.batch_service.core.exception.GroupValidationException;
 import ru.axiomatika.batch_service.web.dto.error.ExceptionResponseDto;
+import ru.axiomatika.batch_service.web.dto.error.GroupExceptionResponseDto;
 import ru.axiomatika.batch_service.web.mapper.error.ExceptionMapper;
 
 @RestControllerAdvice
@@ -14,6 +16,15 @@ import ru.axiomatika.batch_service.web.mapper.error.ExceptionMapper;
 public class GlobalExceptionHandler {
 
     private final ExceptionMapper exceptionMapper;
+
+    @ExceptionHandler(GroupValidationException.class)
+    public ResponseEntity<GroupExceptionResponseDto> handleGroupValidationException(
+            GroupValidationException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exceptionMapper.toGroupExceptionResponse(ex.getExceptions()));
+    }
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ExceptionResponseDto> handleBaseException(BaseException e) {
