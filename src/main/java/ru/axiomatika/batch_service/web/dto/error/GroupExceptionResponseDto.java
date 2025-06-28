5c2dc53ff6b44911f8c74ec17d8ab8bb36dd6719
shustov-dmitry <shustov.dm.s@gmail.com>
2025-06-28 13:14:09 +0300

    feat: add group exception dto

---
+++ src/main/java/ru/axiomatika/batch_service/web/dto/error/GroupExceptionResponseDto.java
@@ -0,0 +1,19 @@
+package ru.axiomatika.batch_service.web.dto.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupExceptionResponseDto {
+
+    private String message;
+    private List<ExceptionResponseDto> errors;
+
+}
