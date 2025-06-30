700bb40a4ad99cd412b4aed16a30854745785a35
shustov-dmitry <shustov.dm.s@gmail.com>
2025-06-30 19:26:43 +0300

    feat: add hash in batch table

--- src/main/resources/db/changelog/changeset/0000__init_batch_requests_table.sql
+++ src/main/resources/db/changelog/changeset/0000__init_batch_requests_table.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS batch_requests (
     id BIGSERIAL PRIMARY KEY,
+    hash VARCHAR(64) NOT NULL,
+    next_attempt TIMESTAMP,
     request_time TIMESTAMP NOT NULL,
-    name VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
     total_requests INT NOT NULL,
     status VARCHAR(20) NOT NULL
 );
