5e1ffb9c43af918e99ac2a2facc7c2cd6ab80314
shustov-dmitry <shustov.dm.s@gmail.com>
2025-06-30 10:28:15 +0300

    feat: process each xml item

--- src/main/java/ru/axiomatika/batch_service/core/service/BatchService.java
+++ src/main/java/ru/axiomatika/batch_service/core/service/BatchService.java
@@ -1,12 +1,22 @@
 package ru.axiomatika.batch_service.core.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.axiomatika.batch_service.core.entity.Batch;
+import ru.axiomatika.batch_service.core.entity.BatchItem;
+import ru.axiomatika.batch_service.core.exception.BaseException;
+import ru.axiomatika.batch_service.core.exception.BaseExceptionCode;
 import ru.axiomatika.batch_service.core.repository.BatchRepository;
 import ru.axiomatika.batch_service.core.validator.BatchValidator;
+import ru.axiomatika.batch_service.web.mapper.BatchItemMapper;
 import ru.axiomatika.batch_service.web.mapper.BatchMapper;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +25,8 @@ public class BatchService {
     private final BatchValidator batchValidator;
     private final BatchMapper batchMapper;
     private final BatchRepository batchRepository;
+    private final BatchItemService batchItemService;
+    private final BatchItemMapper batchItemMapper;
 
     public void processArchive(MultipartFile file) {
         batchValidator.validateArchive(file);
@@ -22,11 +34,29 @@ public class BatchService {
         Batch batch = batchMapper.toBatch(file);
         batchRepository.save(batch);
 
-        processArchiveContent(file);
+        processArchiveContent(file, batch);
     }
 
-    private void processArchiveContent(MultipartFile file) {
+    private void processArchiveContent(MultipartFile file, Batch batch) {
+        try (ZipInputStream zipInputStream = new ZipInputStream(file.getInputStream())) {
+            ZipEntry zipEntry;
+            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                if (!zipEntry.isDirectory()) {
+                    String xmlContent = new String(zipInputStream.readAllBytes(), StandardCharsets.UTF_8);
 
+                    BatchItem batchItem = batchItemMapper.toBatchItem(zipEntry.getName(), xmlContent, batch);
+
+                    batchItemService.save(batchItem);
+                }
+                zipInputStream.closeEntry();
+            }
+        } catch (IOException e) {
+            throw new BaseException(
+                    e.getMessage(),
+                    BaseExceptionCode.INTERNAL_EXCEPTION,
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
 }
