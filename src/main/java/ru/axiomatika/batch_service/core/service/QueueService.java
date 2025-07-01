e9463b6236d8fa7fcc366c8a1561e00268bcacc6
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-01 11:37:34 +0300

    feat: add service

---
+++ src/main/java/ru/axiomatika/batch_service/core/service/QueueService.java
@@ -0,0 +1,19 @@
+package ru.axiomatika.batch_service.core.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.axiomatika.batch_service.core.entity.Batch;
+import ru.axiomatika.batch_service.core.entity.queue.BatchQueueItem;
+import ru.axiomatika.batch_service.core.repository.QueueRepository;
+
+@Service
+@RequiredArgsConstructor
+public class QueueService {
+
+    private final QueueRepository queueRepository;
+
+    public void save(BatchQueueItem queueItem) {
+        queueRepository.save(queueItem);
+    }
+
+}
