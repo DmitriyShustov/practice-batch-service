b462e37030f6a35c99f71479f2c83e658de31665
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-01 11:25:23 +0300

    feat: add field in batch_request

--- src/main/resources/db/changelog/changeset/0000__init_batch_requests_table.sql
+++ src/main/resources/db/changelog/changeset/0000__init_batch_requests_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS batch_requests (
     id BIGSERIAL PRIMARY KEY,
     hash VARCHAR(64) NOT NULL,
+    previous_attempt TIMESTAMP,
     next_attempt TIMESTAMP,
     request_time TIMESTAMP NOT NULL,
     name VARCHAR(255) NOT NULL,
