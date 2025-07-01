1cba32c99dd3c12feec6d4de50c06203e12d43ef
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-01 11:37:56 +0300

    feat: save queue items

--- src/main/java/ru/axiomatika/batch_service/core/service/BatchItemService.java
+++ src/main/java/ru/axiomatika/batch_service/core/service/BatchItemService.java
@@ -3,19 +3,24 @@ package ru.axiomatika.batch_service.core.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.axiomatika.batch_service.core.entity.BatchItem;
+import ru.axiomatika.batch_service.core.entity.queue.BatchQueueItem;
 import ru.axiomatika.batch_service.core.repository.BatchItemRepository;
-import ru.axiomatika.batch_service.web.mapper.BatchItemMapper;
+import ru.axiomatika.batch_service.web.mapper.BatchQueueItemMapper;
 
 @Service
 @RequiredArgsConstructor
 public class BatchItemService {
 
     private final BatchItemRepository batchItemRepository;
+    private final QueueService queueService;
+    private final BatchQueueItemMapper queueItemMapper;
 
     public void save(BatchItem batchItem) {
         batchItemRepository.save(batchItem);
 
-//        add to queue
+        BatchQueueItem queueItem =  queueItemMapper.toBatchQueueItem(batchItem);
+
+        queueService.save(queueItem);
     }
 
 }
