72cbfe53af884693a199ce331ae225f0e6e51594
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-01 12:09:46 +0300

    chore: remove unused dep

--- src/main/java/ru/axiomatika/batch_service/core/exception/DatabaseException.java
+++ src/main/java/ru/axiomatika/batch_service/core/exception/DatabaseException.java
@@ -1,7 +1,6 @@
 package ru.axiomatika.batch_service.core.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 
 public class DatabaseException extends BaseException {
 
