baa2af15957a2ed2ce7f8e873881633948e6f92f
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-02 11:26:55 +0300

    feat: update get processing method

--- src/main/java/ru/axiomatika/batch_service/web/controller/BatchController.java
+++ src/main/java/ru/axiomatika/batch_service/web/controller/BatchController.java
@@ -6,8 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.axiomatika.batch_service.core.entity.Batch;
+import ru.axiomatika.batch_service.core.entity.BatchProcessing;
+import ru.axiomatika.batch_service.core.service.BatchProcessingService;
 import ru.axiomatika.batch_service.core.service.BatchService;
+import ru.axiomatika.batch_service.web.dto.GetBatchProcessingDto;
 import ru.axiomatika.batch_service.web.dto.UploadBatchDto;
+import ru.axiomatika.batch_service.web.mapper.BatchProcessingMapper;
 import ru.axiomatika.batch_service.web.mapper.UploadBatchMapper;
 
 @RestController
@@ -16,7 +20,9 @@ import ru.axiomatika.batch_service.web.mapper.UploadBatchMapper;
 public class BatchController {
 
     private final BatchService batchService;
+    private final BatchProcessingService batchProcessingService;
     private final UploadBatchMapper uploadBatchMapper;
+    private final BatchProcessingMapper batchProcessingMapper;
 
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<UploadBatchDto> uploadZipStream(
@@ -28,8 +34,10 @@ public class BatchController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Void> getBatchProcessingProgress(@PathVariable Long id) {
-        return ResponseEntity.ok(null);
+    public ResponseEntity<GetBatchProcessingDto> getBatchProcessingProgress(@PathVariable Long id) {
+        BatchProcessing batchProcessing = batchProcessingService.getProgress(id);
+
+        return ResponseEntity.ok(batchProcessingMapper.toGetDto(batchProcessing));
     }
 
 }
