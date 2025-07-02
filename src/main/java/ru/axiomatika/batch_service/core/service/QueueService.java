873841d4f78fffa5d3b6986f241275fa36143961
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-02 11:27:42 +0300

    chore: remove unused deps

--- src/main/java/ru/axiomatika/batch_service/core/service/QueueService.java
+++ src/main/java/ru/axiomatika/batch_service/core/service/QueueService.java
@@ -1,17 +1,10 @@
 package ru.axiomatika.batch_service.core.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import ru.axiomatika.batch_service.core.entity.Batch;
-import ru.axiomatika.batch_service.core.entity.BatchStatus;
 import ru.axiomatika.batch_service.core.entity.queue.BatchQueueItem;
-import ru.axiomatika.batch_service.core.exception.BaseException;
-import ru.axiomatika.batch_service.core.exception.BaseExceptionCode;
-import ru.axiomatika.batch_service.core.repository.BatchRepository;
 import ru.axiomatika.batch_service.core.repository.QueueRepository;
 
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
