69286472a093d5e82210b38bdb9cb7104d9a6fb9
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-02 08:45:38 +0300

    feat: add processing repository

---
+++ src/main/java/ru/axiomatika/batch_service/core/repository/BatchProcessingRepository.java
@@ -0,0 +1,52 @@
+package ru.axiomatika.batch_service.core.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+import ru.axiomatika.batch_service.core.entity.BatchProcessing;
+import ru.axiomatika.batch_service.core.exception.DatabaseException;
+
+@Repository
+@RequiredArgsConstructor
+public class BatchProcessingRepository {
+
+    private final SessionFactory sessionFactory;
+
+    public void saveOrUpdate(BatchProcessing processing) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+
+            if (processing.getId() != null) {
+                session.merge(processing);
+            } else {
+                session.persist(processing);
+            }
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DatabaseException("Failed to save or update batch processing");
+        } finally {
+            session.close();
+        }
+    }
+
+    public BatchProcessing findByBatchId(Long batchId) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery(
+                            "SELECT p FROM BatchProcessing p WHERE p.batch.id = :batchId",
+                            BatchProcessing.class)
+                    .setParameter("batchId", batchId)
+                    .uniqueResult();
+        } catch (Exception e) {
+            throw new DatabaseException("Failed to find batch processing by batch id: " + batchId);
+        }
+    }
+
+}
