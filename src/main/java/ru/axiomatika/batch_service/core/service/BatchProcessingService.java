abced2f989176526162f22beab0f6581d78a5a1e
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-02 11:26:30 +0300

    feat: update batch processing in db

--- src/main/java/ru/axiomatika/batch_service/core/service/BatchProcessingService.java
+++ src/main/java/ru/axiomatika/batch_service/core/service/BatchProcessingService.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import ru.axiomatika.batch_service.core.entity.Batch;
+import ru.axiomatika.batch_service.core.entity.BatchProcessing;
 import ru.axiomatika.batch_service.core.entity.BatchStatus;
 import ru.axiomatika.batch_service.core.exception.BaseException;
 import ru.axiomatika.batch_service.core.exception.BaseExceptionCode;
+import ru.axiomatika.batch_service.core.exception.BatchNotFoundException;
+import ru.axiomatika.batch_service.core.repository.BatchProcessingRepository;
 import ru.axiomatika.batch_service.core.repository.BatchRepository;
 
 import java.time.LocalDateTime;
@@ -18,10 +21,11 @@ public class BatchProcessingService {
     private static final int REQUEST_SEC_INTERVAL_FOR_SAME_BATCH = 10;
 
     private final BatchRepository batchRepository;
+    private final BatchProcessingRepository batchProcessingRepository;
 
     public void processBatch(Batch batch) {
         try {
-            updateTimestamps(batch);
+            prepareForUpdate(batch);
 
             Thread.sleep(3500);
 
@@ -36,11 +40,43 @@ public class BatchProcessingService {
         }
     }
 
+    private void prepareForUpdate(Batch batch) {
+        batch.setStatus(BatchStatus.PROCESSING);
+        batchRepository.updateStatus(batch);
+        saveOrUpdateProcessing(getBatchProcessing(batch));
+        updateTimestamps(batch);
+    }
+
     private void updateTimestamps(Batch batch) {
         batch.setPreviousAttempt(LocalDateTime.now());
         batch.setNextAttempt(batch.getPreviousAttempt().plusSeconds(REQUEST_SEC_INTERVAL_FOR_SAME_BATCH));
 
         batchRepository.updateProcessingTimestamps(batch);
+    }
+
+    private BatchProcessing getBatchProcessing(Batch batch) {
+        BatchProcessing batchProcessingByBatchId = batchProcessingRepository.findByBatchId(batch.getId());
+
+        if (batchProcessingByBatchId == null) {
+            batchProcessingByBatchId = BatchProcessing.createStartBatchProcessing(batch);
+        }
+
+        return batchProcessingByBatchId;
+    }
+
+    private void saveOrUpdateProcessing(BatchProcessing batchProcessing) {
+
+        batchProcessingRepository.saveOrUpdate(batchProcessing);
+    }
+
+    public BatchProcessing getProgress(Long batchId) {
+        BatchProcessing batchProcessing = batchProcessingRepository.findByBatchId(batchId);
+
+        if (batchProcessing == null) {
+            throw new BatchNotFoundException(batchId);
+        }
+
+        return batchProcessing;
     }
 
 }
