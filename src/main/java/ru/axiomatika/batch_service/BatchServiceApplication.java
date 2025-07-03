09c434cf4d5a08b14aa155ad2e244c25082e90b1
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-03 11:29:23 +0300

    fix: add annotation to app

--- src/main/java/ru/axiomatika/batch_service/BatchServiceApplication.java
+++ src/main/java/ru/axiomatika/batch_service/BatchServiceApplication.java
@@ -2,10 +2,12 @@ package ru.axiomatika.batch_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"ru.axiomatika.batch_service.web.mappers", "ru.axiomatika.batch_service"})
+@EnableFeignClients(basePackages = "ru.axiomatika.batch_service.core.request")
 public class BatchServiceApplication {
 
 	public static void main(String[] args) {
