f03bb91a50ee202bf7761e0336317fe7042d5095
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-04 16:15:15 +0300

    feat: add new config

--- src/main/java/ru/axiomatika/batch_service/core/config/BatchProcessingConfig.java
+++ src/main/java/ru/axiomatika/batch_service/core/config/BatchProcessingConfig.java
@@ -11,16 +11,22 @@ public class BatchProcessingConfig {
     public final int XML_FILES_PROCESSING_AMOUNT_PER_ONE_TIME;
     public final int XML_FILES_PROCESSING_INTERVAL_MS;
     public final int XML_FILES_PROCESSING_INITIAL_DELAY_MS;
+    public final int XML_FILES_PROCESSING_TIME_FOR_REPEAT_ERROR_REQUESTS_MS;
+    public final int XML_FILES_PROCESSING_MAX_TRY_COUNT;
 
     public BatchProcessingConfig(
             @Value("${archive.processing.request-interval-sec}") int requestInterval,
             @Value("${archive.processing.portion-size}") int portionSize,
             @Value("${archive.processing.portion-interval-ms}") int portionInterval,
-            @Value("${archive.processing.initial-delay-ms}") int initialDelay) {
+            @Value("${archive.processing.initial-delay-ms}") int initialDelay,
+            @Value("${archive.processing.repeat-error-request-ms}") int repeatErrorRequest,
+            @Value("${archive.processing.try-count}") int tryCount) {
         this.REQUEST_INTERVAL_FOR_SAME_BATCH_SEC = requestInterval;
         this.XML_FILES_PROCESSING_AMOUNT_PER_ONE_TIME = portionSize;
         this.XML_FILES_PROCESSING_INTERVAL_MS = portionInterval;
         this.XML_FILES_PROCESSING_INITIAL_DELAY_MS = initialDelay;
+        this.XML_FILES_PROCESSING_TIME_FOR_REPEAT_ERROR_REQUESTS_MS = repeatErrorRequest;
+        this.XML_FILES_PROCESSING_MAX_TRY_COUNT = tryCount;
     }
 
 }
