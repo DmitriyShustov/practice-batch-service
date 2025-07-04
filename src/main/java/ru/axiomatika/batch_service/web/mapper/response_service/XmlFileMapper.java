aa1862c02434334efb0d4359a2c631bc3738f4ff
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-04 16:13:41 +0300

    feat: add xml mapper

---
+++ src/main/java/ru/axiomatika/batch_service/web/mapper/response_service/XmlFileMapper.java
@@ -0,0 +1,14 @@
+package ru.axiomatika.batch_service.web.mapper.response_service;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.axiomatika.batch_service.web.dto.QueueAndBatchItemDto;
+import ru.axiomatika.batch_service.web.dto.response_service.XmlFileDto;
+
+@Mapper(componentModel = "spring")
+public interface XmlFileMapper {
+
+    @Mapping(source = "batchItem.name", target = "name")
+    @Mapping(source = "batchItem.xmlContent", target = "xmlData")
+    XmlFileDto toDto(QueueAndBatchItemDto queueAndBatchItemDto);
+}
