bd79f09f857168831309a6af5b14bb7c667862ad
shustov-dmitry <shustov.dm.s@gmail.com>
2025-07-04 10:26:20 +0300

    feat: add setting FAILED status

--- src/main/java/ru/axiomatika/batch_service/core/service/QueueService.java
+++ src/main/java/ru/axiomatika/batch_service/core/service/QueueService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.axiomatika.batch_service.core.entity.Batch;
 import ru.axiomatika.batch_service.core.entity.BatchStatus;
 import ru.axiomatika.batch_service.core.entity.queue.BatchQueueItem;
+import ru.axiomatika.batch_service.core.exception.InterruptBatchProcessingException;
 import ru.axiomatika.batch_service.core.repository.BatchRepository;
 import ru.axiomatika.batch_service.core.repository.QueueRepository;
 
@@ -21,8 +22,14 @@ public class QueueService {
     }
 
     public void performRequests(Batch batch) {
-        batch.setStatus(BatchStatus.COMPLETED);
-        batchRepository.updateStatus(batch);
+        try {
+            performRequest();
+
+        } catch (Exception e) {
+            batch.setStatus(BatchStatus.FAILED);
+            batchRepository.updateStatus(batch);
+            throw new InterruptBatchProcessingException(e.getMessage());
+        }
     }
 
     private void performRequest() {
